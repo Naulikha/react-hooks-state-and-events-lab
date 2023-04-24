cbdc39186cfbbae33ad85bbb2341d2a1d4e4a072
Naulikha <nbiketia@gmail.com>
2023-04-24 04:09:31 +0300

    Completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+    const [selectedCategory, setSelectedCategory] = useState("All");
+  
+    function handleCategoryChange(event) {
+      setSelectedCategory(event.target.value);
+    }
+  
+    // filter items based on selected category
+    const filteredItems = selectedCategory === "All"
+      ? items
+      : items.filter(item => item.category === selectedCategory);
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
